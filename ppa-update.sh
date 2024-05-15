f684ed71cb0f5e43ccbd6b0880d1577621d10866
Sergey <mail@sb.stumasov.ru>
2024-05-15 23:46:43 +0300

    update

---
+++ ppa-update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+apt-ftparchive release . > Release
+gpg --default-key "${EMAIL}" -abs -o - Release > Release.gpg
+gpg --default-key "${EMAIL}" --clearsign -o - Release > InRelease
+
+git add -A
+git commit -m "update"
+git push -u origin master
+
+
