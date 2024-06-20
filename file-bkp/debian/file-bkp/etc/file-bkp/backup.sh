616b1758f1fbb676bdcdc966e075a76b3023b18f
Sergey <mail@sb.stumasov.ru>
2024-06-20 13:27:57 +0300

    update

--- file-bkp/debian/file-bkp/etc/file-bkp/backup.sh
+++ file-bkp/debian/file-bkp/etc/file-bkp/backup.sh
@@ -2,13 +2,23 @@
 
 LOG_PATH="/var/log/file-bkp.log"
 
-INC_SRC_LIST="/etc/file-bkp/inc_files.list"
-EXC_SRC_LIST="/etc/file-bkp/exc_files.list"
+WORK_DIR="/etc/file-bkp"
 
-LAST_BKP=`tac "$LOG_PATH" | grep -m1 "created directory" | sed -r 's/^.*created directory //'`
+#DB backup
+MYSQL_BKP_FILE="${WORK_DIR}/$(date +%Y-%m-%d_%H-%M)-mysql-all-databases.sql.lzma"
+
+if [ $(systemctl is-active mysql) == 'active' ]
+then
+        mysqldump -u bkp_user -p123 --all-databases | lzma > "${MYSQL_BKP_FILE}"
+fi
+
+INC_SRC_LIST="${WORK_DIR}/inc_files.list"
+EXC_SRC_LIST="${WORK_DIR}/exc_files.list"
+
+RSYNC_LAST_BKP=`tac "$LOG_PATH" | grep -m1 "created directory" | sed -r 's/^.*created directory //'`
 OPT="-arvHAX"
 OPT+=" --log-file=${LOG_PATH}"
-[ -z $LAST_BKP ] || OPT+=" --link-dest=../..${LAST_BKP}"
+[ -z $RSYNK_LAST_BKP ] || OPT+=" --link-dest=../..${RSYNC_LAST_BKP}"
 OPT+=" --files-from=${INC_SRC_LIST} --exclude-from=${EXC_SRC_LIST}"
 
 DEST_SERV='rsync://bkp.sb.stumasov.ru'
@@ -19,4 +29,10 @@ rsync -avHAX --filter="- *" --filter="- */" / "${DEST_SERV}/backup/$(hostname)"
 
 #backup data
 rsync `echo "${OPT}"` "/"  "${DEST_SERV}/${DEST_DIR}"
+
+#remove mysql backup file
+if [ -f "${MYSQL_BKP_FILE}" ]
+then
+        rm -f "${MYSQL_BKP_FILE}"
+fi
 
